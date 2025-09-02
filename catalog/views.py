2c3ce2bf01c738d8e236d04295eb77867283bf34
Gridnev Nikita <gridnevnikit4@gmail.com>
2025-09-02 20:01:33 +0300

    project fixed

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        user_message = request.POST.get('message')
+        print(f'You have new message from {name}({phone}): {user_message}')
+        return render(request, 'catalog/thanks.html', {'name': name})
+    return render(request, 'catalog/contacts.html')
